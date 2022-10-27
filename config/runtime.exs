d90e1d270fd90e52e1f4a9885c9ef8c8cff8afde
BJ Bryant <bbryant@covermymeds.com>
2022-10-26 22:50:54 -0500

    Typo

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,7 +54,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port: String.to_integer(System.get_env("HTTP_PORT"))
+      port: String.to_integer(System.get_env("HTTP_PORT"))
     ],
     https: [port: String.to_integer(System.get_env("HTTPS_PORT"))],
     secret_key_base: secret_key_base
